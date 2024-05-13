02a5491e5ac220635fa2f017223ef8c94b9acd7d
Rupanshu Kapoor <rupanshukapoor@outlook.com>
2024-05-13 23:05:06 +0530

    Add hyperparameter for logistic and Xgboost

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.model_selection import GridSearchCV
 from joblib import dump  # For saving models
 from config import model_dict
-
+import streamlit as st
 class ModelTrainer:
     def __init__(self, json_content: dict):
         self.json_content = json_content
@@ -37,7 +37,7 @@ class ModelTrainer:
         gcv.fit(X_train, y_train)
         return gcv.best_estimator_
 
-    def tune_linear_logistic_elasticnet_regression(self, model, X_train, y_train, model_name, model_parameters):
+    def tune_linear_elasticnet_regression(self, model, X_train, y_train, model_name, model_parameters):
         params = {"random_state": self.random_state}
         if model_parameters[model_name].get("random_state"):
             params["random_state"] = model_parameters[model_name]["random_state"]
@@ -59,6 +59,28 @@ class ModelTrainer:
         return gcv.best_estimator_
 
   
+    
+    def tune_logistic_regression(self, model, X_train, y_train, model_parameters):
+        params = {"random_state": self.random_state}
+        if model_parameters["LogisticRegression"].get("random_state"):
+            params["random_state"] = model_parameters["LogisticRegression"]["random_state"]
+        
+        min_iter = model_parameters["LogisticRegression"]["min_iter"]
+        max_iter = model_parameters["LogisticRegression"]["max_iter"]
+        params["max_iter"] = np.linspace(min_iter, max_iter, self.num_iter, dtype=int)
+
+        min_reg = model_parameters["LogisticRegression"]["min_regparam"]
+        max_reg = model_parameters["LogisticRegression"]["max_regparam"]
+        params["C"] = np.logspace(min_reg, max_reg, self.num_iter)
+
+        min_elasticnet = model_parameters["LogisticRegression"]["min_elasticnet"]
+        max_elasticnet = model_parameters["LogisticRegression"]["max_elasticnet"]
+        params["l1_ratio"] = np.linspace(min_elasticnet, max_elasticnet, self.num_iter)
+
+        gcv = GridSearchCV(model, params, cv=self.k_fold)
+        gcv.fit(X_train, y_train)
+        return gcv.best_estimator_
+
     def tune_ridge_lasso_regression(self, model, X_train, y_train, model_name, model_parameters):
         params = {"random_state": self.random_state}
         if model_parameters[model_name].get("random_state"):
@@ -108,9 +130,7 @@ class ModelTrainer:
         return gcv.best_estimator_
     
     def tune_svm(self, model, X_train, y_train, model_parameters):
-        params = {"random_state": self.random_state}
-        if model_parameters["SVM"].get("random_state"):
-            params["random_state"] = model_parameters["SVM"]["random_state"]
+        params = {}
         
         kernel = []
         if model_parameters["SVM"]["linear_kernel"]:
@@ -141,14 +161,12 @@ class ModelTrainer:
         return gcv.best_estimator_
 
     def tune_knn(self, model, X_train, y_train, model_parameters):
-        params = {"random_state": self.random_state}
-        if model_parameters["KNN"].get("random_state"):
-            params["random_state"] = model_parameters["KNN"]["random_state"]
+        params = {}
         
         params["n_neighbors"] = model_parameters["KNN"]["k_value"]
 
         if model_parameters["KNN"].get("distance_weighting"):
-            params["weights"] = "distance"
+            params["weights"] = ["distance"]
 
         if model_parameters["KNN"]["neighbour_finding_algorithm"] == "Automatic":
             params["algorithm"] = "auto"
@@ -157,6 +175,7 @@ class ModelTrainer:
         gcv.fit(X_train, y_train)
         return gcv.best_estimator_
         pass
+
 
     def tune_neural_network(self, model, X_train, y_train, model_parameters):
         parameters = model_parameters["neural_network"]
@@ -189,12 +208,32 @@ class ModelTrainer:
         return gcv.best_estimator_
     
     def tune_xgb(self, model, X_train, y_train, model_name, model_parameters):
-        params = {"random_state": self.random_state
-                  
+        params = {"random_state": self.random_state,
+                  "booster": []
                   }
-        if model_parameters["XGB"].get("random_state"):
-            params["random_state"] = model_parameters["XGB"]["random_state"]
-        
+        if model_parameters["xg_boost"].get("random_state"):
+            params["random_state"] = model_parameters["xg_boost"]["random_state"]
+
+        if model_parameters["xg_boost"].get("use_gradient_boosted_tree"):
+            params["booster"].append("gbtree")
+
+        if model_parameters["xg_boost"].get("dart"):
+            params["booster"].append("dart")
+
+        params["n_estimators"] = model_parameters["xg_boost"]["max_num_of_trees"]
+        params["tree_method"] = model_parameters["xg_boost"]["tree_method"]
+        if model_parameters["xg_boost"]["early_stopping"]:
+            params["early_stopping_rounds"] = model_parameters["xg_boost"]["early_stopping_rounds"]
+
+        params["max_depth"] = model_parameters["xg_boost"]["max_depth_of_tree"]
+        params["learning_rate"] = model_parameters["xg_boost"]["learningRate"]
+        params["l1_regularization"] = model_parameters["xg_boost"]["l1_regularization"]
+        params["l2_regularization"] = model_parameters["xg_boost"]["l2_regularization"]
+        params["min_child_weight"] = model_parameters["xg_boost"]["min_child_weight"]
+        params["gamma"] = model_parameters["xg_boost"]["gamma"]
+        params["sub_sample"] = model_parameters["xg_boost"]["sub_sample"]
+        params["col_sample_by_tree"] = model_parameters["xg_boost"]["col_sample_by_tree"]
+
         gcv = GridSearchCV(model, params, cv=self.k_fold)
         gcv.fit(X_train, y_train)
         return gcv.best_estimator_
@@ -203,6 +242,9 @@ class ModelTrainer:
     def build_and_tune_model(self, X_train, y_train, problem_type, selected_models, model_parameters):
         self.best_models = {}
         for model_name in selected_models:
+            if model_name == "xg_boost":
+                st.warning("As of now xg_boost is not supported")
+                continue
             if model_name == "SVM" and problem_type == "Regression":
                 model = model_dict["SVMRegressor"]
                 best_model = self.tune_svm(model, X_train, y_train, model_parameters)
@@ -229,18 +271,23 @@ class ModelTrainer:
 
             elif model_name == "xg_boost" and problem_type == "Regression":
                 model = model_dict["XGBoostRegressor"]
+                best_model = self.tune_xgb(model, X_train, y_train, model_name, model_parameters)
+
             elif model_name == "xg_boost" and problem_type == "Classification":
                 model = model_dict["XGBoostClassifier"]
+                best_model = self.tune_xgb(model, X_train, y_train, model_name, model_parameters)
             else:
                 model = model_dict[model_name]
 
             if (model_name == "RandomForestClassifier" or model_name == "RandomForestRegressor"):
                 best_model = self.tune_random_forest(model, X_train, y_train, model_name, model_parameters)
             
-            elif (model_name == "LinearRegression" or model_name == "LogisticRegression" 
-                  or model_name == "ElasticNetRegression"):
-                best_model = self.tune_linear_logistic_elasticnet_regression(model, X_train, y_train, model_name, model_parameters)
+            elif (model_name == "LinearRegression" or  model_name == "ElasticNetRegression"):
+                best_model = self.tune_linear_elasticnet_regression(model, X_train, y_train, model_name, model_parameters)
             
+            elif model_name == "LogisticRegression":
+                best_model = self.tune_logistic_regression(model, X_train, y_train, model_parameters)
+
             elif (model_name == "RidgeRegression" or model_name == "LassoRegression"):
                 best_model = self.tune_ridge_lasso_regression(model, X_train, y_train, model_name, model_parameters)
             
